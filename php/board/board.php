99311de6dfc2ed0b0aa61302f187c8668093187c
kiwowki <wolves941110@gmail.com>
2023-10-05 19:25:02 +0900

    2023.10.05 수업정리
    php, 프로젝트2 인트로?

--- php/board/board.php
+++ php/board/board.php
@@ -2,9 +2,9 @@
     include "../connect/connect.php";
     include "../connect/session.php";
 
-    echo "<pre>";
-    var_dump($_SESSION);
-    echo "</pre>";
+    // echo "<pre>";
+    // var_dump($_SESSION);
+    // echo "</pre>";
 ?>
 <!DOCTYPE html>
 <html lang="ko">
@@ -22,6 +22,16 @@
 
     <?php include "../include/header.php" ?>
     <!-- header -->
+    <?php
+    // 총 페이지 갯수
+    $sql = "SELECT count(boardID) FROM board";
+    $result = $connect -> query($sql);
+
+
+    $boardTotalCount = $result -> fetch_array(MYSQLI_ASSOC);
+    $boardTotalCount = $boardTotalCount['count(boardID)'];
+    echo $boardTotalCount;
+    ?>
 
     <main id="main" role="main">
     <div class="intro_inner container bmStyle">
@@ -36,7 +46,27 @@
             </div>
         </div>
         <section class="board_inner container">
-        <div class="board_table">
+            <div class="board_search">
+                    <div class="left">
+                        * 총 <em><?=$boardTotalCount?></em>건의 게시물이 등록되어 있습니다.
+                    </div>
+                    <div class="right">
+                        <form action="boardSearch.html" name="boardSearch" method="get">
+                            <fieldset>
+                                <legend class="blind">게시판 검색 영역</legend>
+                                <input type="search" name="searchKeyword" id="searchKeyword"    placeholder="검색어를 입력하세요!" required>
+                                <select name="searchOption" id="searchOption">
+                                    <option value="title">제목</option>
+                                    <option value="content">내용</option>
+                                    <option value="name">등록자</option>
+                                </select>
+                                <button type="submit" class="btn_style3">검색</button>
+                                <a href="boardWrite.php">글쓰기</a>
+                            </fieldset>
+                        </form>
+                </div>
+            </div>
+            <div class="board_table">
                 <table>
                     <colgroup>
                         <col style="width: 5%;">
@@ -44,7 +74,6 @@
                         <col style="width: 10%;">
                         <col style="width: 15%;">
                         <col style="width: 7%;">
-                        
                     </colgroup>
                     <thead>
                         <tr>
@@ -55,7 +84,7 @@
                             <th>조회수</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <!-- <tbody>
                         <tr>
                             <td>1</td>
                             <td><a href="boardView.php">게시판 제목</a></td>
@@ -63,8 +92,133 @@
                             <td>2023-04-04</td>
                             <td>100</td>
                         </tr>
+                    </tbody> -->
+                    <tbody>
+<?php
+
+    // 쌤이랑 한 거
+
+    // $sql = "SELECT b.boardID, b.boardTitle, m.youName, b.regTime, b.boardView FROM board b JOIN members m ON(b.memberID = m.memberID) ORDER BY boardID DESC LIMIT 0, 10";
+    // $result = $connect -> query($sql);
+
+    // if($result){
+    //     $count = $result -> num_rows;
+
+    //     if ($count > 0) {
+    //         for($i=0; $i<$count; $i++){
+    //             $info = $result -> fetch_array(MYSQLI_ASSOC);
+
+    //             echo "<tr>";
+    //             echo "<td>".$info['boardID']."</td>";
+    //             echo "<td>".$info['boardTitle']."</td>";
+    //             echo "<td>".$info['youName']."</td>";
+    //             echo "<td>".date('Y-m-d', $info['regTime'])."</td>";
+    //             echo "<td>".$info['boardView']."</td>";
+    //         }
+    //     }
+    // }
+
+    // 챗gpt가 알려준 거.
+    if(isset($_GET['page'])){
+        $page = (int) $_GET['page'];
+    } else {
+        $page = 1;
+    }
+
+    $viewNum = 10;
+    $viewLimit = ($viewNum * $page) - $viewNum;
+
+    // 1~10 LIMIT 0, 10     ==> page1   ($viewNum * 1) - $viewNum
+    // 11~20 LIMIT 10, 10   ==> page2   ($viewNum * 2) - $viewNum
+    // 21~30 LIMIT 20, 10   ==> page3   ($viewNum * 3) - $viewNum
+    // 31~40 LIMIT 30, 10   ==> page4   ($viewNum * 4) - $viewNum
+    
+
+    $sql = "SELECT * FROM board ORDER BY boardID DESC LIMIT {$viewLimit}, {$viewNum}";
+    $result = $connect -> query($sql);
+
+    if($result){
+        $count = $result -> num_rows;
+
+        if($count > 0){
+            for($i=0; $i<$count; $i++){
+                $info = $result -> fetch_array(MYSQLI_ASSOC);
+
+                echo "<tr>";
+                echo "<td>".$info['boardID']."</td>";
+                echo "<td>".$info['boardTitle']."</td>";
+                echo "<td>".$info['youName']."</td>";
+                echo "<td>".date('Y-m-d', $info['regTime'])."</td>";
+                echo "<td>".$info['boardView']."</td>";
+                echo "</tr>";
+            }
+        } else {
+            echo "<tr><td colspan='5'>게시글이 없습니다.</td></tr>";
+        }
+    } else {
+        echo "관리자에게 문의해주세요!";
+    }
+?>
+
                     </tbody>
                 </table>
+            </div>
+            <div class="board_pages">
+                <ul>
+                <li class="first"><a href="board.php?page=1">처음으로</a></li>
+<?php if ($page > 1): ?>
+                        <li class="next"><a href="board.php?page=<?php echo $page - 1; ?>">이전</a></li>
+<?php endif; ?>
+
+<?php
+    // 총 페이지 갯수
+    $boardTotalCount = ceil($boardTotalCount/$viewNum); //반올림 하기
+
+    // 1 2 3 4 5 6 "7" 8 9 10 11 12 -> 
+    $pageView = 5;  // 앞 뒤로 5개씩
+    $startPage = $page - $pageView;
+    $endPage = $page + $pageView;
+
+    // 처음 페이지 초기화/마지막 페이지 초기화
+    if($startPage < 1) $startPage = 1;
+    if($startPage >= $boardTotalCount) $endPage = $boardTotalCount;
+
+    for ($i = $startPage; $i <= $endPage; $i++){
+        $active = "";
+        if($i == $page) $active = "active";
+
+        echo "<li class='{$active}'><a href='board.php?page={$i}'>${i}</a></li>";
+        $endPage = min($startPage + ($pageView * 2), $boardTotalCount);
+    }
+    // 
+    
+    // for ($i = $startPage; $i <= $endPage; $i++) {
+    //     if ($i == $page) {
+    //         // 현재 페이지인 경우
+    //         echo "<li class='active'><a href='#'>$i</a></li>";
+    //     } else {
+    //         // 다른 페이지인 경우
+    //         echo "<li><a href='#'>$i</a></li>";
+    //     }
+    // }
+    // echo "<li class='active'><a href='#'>${page}</a></li>";
+
+?>
+
+                    <!-- <li class="active"><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#">6</a></li>
+                    <li><a href="#">7</a></li> -->
+                    <!-- <li class="next"><a href="board.php?page=<?php echo $page + 1; ?>">다음</a></li> -->
+<?php if ($page < $boardTotalCount): ?>
+                        <li class="next"><a href="board.php?page=<?php echo $page + 1; ?>">다음</a></li>
+<?php endif; ?>
+                    
+                    <li class="last"><a href="board.php?page=<?php echo $boardTotalCount; ?>">마지막으로</a></li>
+                </ul>
             </div>
         </section>
     </main>
